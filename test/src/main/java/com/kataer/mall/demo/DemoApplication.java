19e9671df35a4a4e12fd6ef62be569f0c806d1d0
wangxudream <252830657@qq.com>
2021-02-06 21:25:24 +0800

    1、添加redis依赖

--- test/src/main/java/com/kataer/mall/demo/DemoApplication.java
+++ test/src/main/java/com/kataer/mall/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @ClassName MallPortalApplication
@@ -15,6 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(value = "com.kataer.mall.*")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
