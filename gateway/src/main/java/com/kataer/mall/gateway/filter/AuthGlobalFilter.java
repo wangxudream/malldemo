f6ed6abb06c5345bee35058527efb5f9f3c50cb8
wangxudream <252830657@qq.com>
2021-03-03 19:18:24 +0800

    1、gateway学习

--- gateway/src/main/java/com/kataer/mall/gateway/filter/AuthGlobalFilter.java
+++ gateway/src/main/java/com/kataer/mall/gateway/filter/AuthGlobalFilter.java
@@ -1,14 +1,21 @@
 package com.kataer.mall.gateway.filter;
 
+import com.alibaba.fastjson.JSONObject;
+import com.kataer.mall.common.ResultBody;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 
 /**
@@ -18,30 +25,29 @@ import reactor.core.publisher.Mono;
  * @Date 2021/1/24 21:34
  * @Version V1.0
  **/
-//@Component
+@Slf4j
+@Component
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
     /**
      * 认证信息Http请求头
      */
-    private static final String JWT_TOKEN_HEADER = "Authorization";
+    public static final String LOGIN_TOKEN_KEY = "X-PointsShop-Token";
 
-    /**
-     * JWT令牌前缀
-     */
-    private static final String JWT_TOKEN_PREFIX = "Bearer ";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        String token = exchange.getRequest().getHeaders().getFirst(JWT_TOKEN_HEADER);
+        String token = exchange.getRequest().getHeaders().getFirst(LOGIN_TOKEN_KEY);
         if (StringUtils.isEmpty(token)) {
-            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-            return chain.filter(exchange);
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            byte[] bytes = JSONObject.toJSONString(ResultBody.unauthorized()).getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = response.bufferFactory().wrap(bytes);
+            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            return response.writeWith(Mono.just(buffer));
         }
-
-        return null;
+        return chain.filter(exchange);
     }
 
-    //过滤器的顺序
     @Override
     public int getOrder() {
         return 0;
